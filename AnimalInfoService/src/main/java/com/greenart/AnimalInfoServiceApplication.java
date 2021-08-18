5c8f501cdf13659ea417d336cde1dcf27d9b80dc
HarryBonoPark <hyery95@naver.com>
2021-08-18 16:41:02 +0900

    유기동물 조회 프로젝트 생성

---
+++ AnimalInfoService/src/main/java/com/greenart/AnimalInfoServiceApplication.java
@@ -0,0 +1,13 @@
+package com.greenart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnimalInfoServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnimalInfoServiceApplication.class, args);
+	}
+
+}
