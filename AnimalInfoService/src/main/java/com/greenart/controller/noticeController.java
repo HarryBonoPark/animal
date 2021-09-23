5e8d2ab91c005a052965080ca2390ce849a068f0
2gahyun <laurelnike@gmail.com>
2021-09-23 16:30:17 +0900

    이가현 공지사항 세션 접속 오류 완료

--- AnimalInfoService/src/main/java/com/greenart/controller/noticeController.java
+++ AnimalInfoService/src/main/java/com/greenart/controller/noticeController.java
@@ -6,10 +6,8 @@ import javax.servlet.http.HttpSession;
 
 import com.greenart.service.CategoryService;
 import com.greenart.service.MemberInfoService;
-import com.greenart.service.NoticeAdminService;
 import com.greenart.service.NoticeService;
 import com.greenart.vo.CategoryVO;
-import com.greenart.vo.MemberInfoVO;
 import com.greenart.vo.NoticeVO;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,7 +22,6 @@ public class noticeController {
     @Autowired CategoryService c_service;
     @Autowired NoticeService n_service;
     @Autowired MemberInfoService m_service;
-    @Autowired NoticeAdminService ma_service;
 
     @GetMapping("/notice")
     public String getNotice(@RequestParam @Nullable Integer seq, @RequestParam @Nullable Integer status, Model model){
@@ -43,16 +40,6 @@ public class noticeController {
     public String getNoticeList(@RequestParam @Nullable Integer seq, HttpSession session, Model model){
         List<CategoryVO> clist = c_service.selectCategoryAll();
         model.addAttribute("clist", clist);
-        // NoticeVO notice = n_service.selectNoticeByseq(seq);
-        // 여기에서 공지사항 목록을 model로 내보내주세요.
-        MemberInfoVO member_info = (MemberInfoVO)session.getAttribute("member");
-        if(member_info == null){
-            return "/notice/nList";
-        }
-        Integer mi_status = member_info.getStatus();
-        
-        MemberInfoVO status = ma_service.selectAdminStatus(mi_status);
-        model.addAttribute("member_info", status);
         NoticeVO notice = n_service.selectNoticeByseq(seq);
         model.addAttribute("notice", notice);
         List<NoticeVO> nList = n_service.selectAllNotice();
